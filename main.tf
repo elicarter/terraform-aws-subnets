901b48a5407a8bdb461c70b5b955cc75cabfe4f9
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 09:09:56 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
 
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = var.existing_vpc_id
+  cidr_block = cidrsubnet(var.subnet_cidr, 4, count.index)
+  count = length(var.availability_zones)
+
+  availability_zone = var.availability_zones[count.index]
+}
