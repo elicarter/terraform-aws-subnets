5c9a3b26717be57e571326edfe24f5310e189712
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 09:30:57 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "my_subnet" {
   vpc_id     = var.existing_vpc_id
   cidr_block = cidrsubnet(var.subnet_cidr, 4, count.index)
   count = length(var.availability_zones)
-
+}
 variable "availability_zones" {
   type    = list(string)
   default = ["us-west-2a", "us-west-2b"] 
